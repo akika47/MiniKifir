1262f5e7bcc6dd5445de165d1c99d023351e816c
akika47 <61090999+akika47@users.noreply.github.com>
2024-02-10 17:39:47 +0100

    responsivitas es csv generalas

--- frontend/Sorting.js
+++ frontend/Sorting.js
@@ -245,9 +245,23 @@ function createTable(amountOfRows, filteredStudents){
 
 const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+var comparer = function(idx, asc) { 
+
+  // This is used by the array.sort() function...
+  return function(a, b) { 
+
+      // This is a transient function, that is called straight away. 
+      // It allows passing in different order of args, based on 
+      // the ascending/descending order.
+      return function(v1, v2) {
+
+          // sort based on a numeric or localeCompare, based on type...
+          return (v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2)) 
+              ? v1 - v2 
+              : v1.toString().localeCompare(v2);
+      }(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+  }
+};
 
 document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
     const table = th.closest('table');
